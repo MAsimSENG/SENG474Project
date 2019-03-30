16b94e30f2b0ee1d2500ccb92d2e71b70153e2aa
Your Name <you@example.com>
2019-03-30 14:52:18 -0700

    added documentation

--- preprocess.py
+++ preprocess.py
@@ -1,10 +1,17 @@
-#Preprocessor methods on the feature matrix
+#Preprocessor methods for the feature matrix
 
 import numpy as np
 from sklearn.decomposition import pca
 
-# x_train is the training data
 def train_normalize(x_train):
+    '''
+    This function finds the mean and std along each dimension of the features
+    matrix for feature normalization.
+
+    :param x_train: ndarray(N,D): train features matrix
+    :return: x_train_mean: ndarray (D,): mean value along each dimension
+             x_train_std: ndarray(D,): standard deviation along each dimension
+    '''
     x_train_mean = np.mean(x_train, axis=0)
     x_train_std = np.std(x_train, axis=0)
 
